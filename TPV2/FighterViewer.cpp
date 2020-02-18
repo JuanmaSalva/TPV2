ccb834f0620d4247a5fae42ee8c95aee91384673
Andres <47799692+AndresRuizB@users.noreply.github.com>
2020-02-18 17:33:04 +0100

    ahora el fighter viewer usa el transform

--- TPV2/FighterViewer.cpp
+++ TPV2/FighterViewer.cpp
@@ -4,6 +4,7 @@ FighterViewer::FighterViewer() :
 	Component(ecs::Fighter)
 {
 	texture = nullptr;
+	transform = nullptr;
 }
 
 FighterViewer::~FighterViewer()
@@ -13,9 +14,11 @@ FighterViewer::~FighterViewer()
 void FighterViewer::init()
 {
 	texture = game_->getTextureMngr()->getTexture(9);
+	transform = entity_->getComponent<Transform>(ecs::Transform);
 }
 
 void FighterViewer::draw()
 {
-	texture->render( SDL_Rect{ 10, 10, 100, 100 }, 45.0, SDL_Rect{47, 90, 207, 250});
+	texture->render( SDL_Rect{ (int)transform->getPos().getX(), (int)transform->getPos().getY(), (int)transform->getH(), (int)transform->getW() },
+					transform->getRot(), SDL_Rect{47, 90, 207, 250});
 }
